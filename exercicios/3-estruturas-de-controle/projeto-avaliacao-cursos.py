8c5db3829ab01dbca2af75fff74ea0fd0a389721
afariasilva <afariasilva@gmail.com>
2025-06-03 18:04:32 +0000

    exercicio avaliação curso

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,24 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+cursos_linkedin = ['Introdução a SQL', 'Python: Formação Básica', 'Python para Ciência de Dados: Formação Básica', 'Descubra a Linguagem de Programação R', 'Fundamentos de Programação: Algoritmos']
+
+curso_git = 'Git e GitHub: Formação Básica'
+curso_python = 'Python: Formação Básica'
+curso_r = 'Descubra a Linguagem de Programação R'
+
+avaliacoes = {}
+
+if curso_python in cursos_linkedin:
+  print(f'O curso "{curso_python}" está no catálogo. Por favor, avalie o curso.')
+  avaliacoes[curso_python] = int(input('Qual nota você dá para esse curso (0 a 5)?'))
+if curso_git in cursos_linkedin:
+  print(f'O curso "{curso_git}" está no catálogo. Por favor, avalie o curso.')
+  avaliacoes[curso_git] = int(input('Qual nota você dá para esse curso (0 a 5)?'))
+if curso_r in cursos_linkedin:
+  print(f'O curso "{curso_r}" está no catálogo. Por favor, avalie o curso.')
+  avaliacoes[curso_r] = int(input('Qual nota você dá para esse curso (0 a 5)?'))
+else:
+  print('Infelizmente o curso não compõe o nosso catálogo.')
+
+print(avaliacoes)
